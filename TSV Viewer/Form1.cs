3e840009f6eb2d75055ff70236f6d1ed03759901
thetornadotitan <thetornadotitan@gmail.com>
2018-06-02 15:34:56 -0500

    Local DB interaction

--- TSV Viewer/Form1.cs
+++ TSV Viewer/Form1.cs
@@ -8,12 +8,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Data.SqlClient;
 
 namespace MediaTracker
 {
     public partial class Form1 : Form
     {
         private string fileLocation = "";
+        private SqlConnection conn;
+        private string query;
+        private SqlCommand cmd;
+        private SqlDataReader rdr;
 
         public Form1()
         {
@@ -22,7 +27,9 @@ namespace MediaTracker
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\db.mdf;Integrated Security=True");
+            conn.Open();
+            conn.Close();
         }
 
         private void ScanBtn_Click(object sender, EventArgs e)
@@ -69,21 +76,59 @@ namespace MediaTracker
                 string[] files = Directory.GetFiles(fileLocation);
                 foreach (string f in files)
                 {
-                    Console.WriteLine(f);
                     EpisodeFlow.Controls.Add(GenerateEpisodeControl(f));
-                    
                 }
             }
             EpisodeFlow.Refresh();
         }
 
-        private void ToggleVisibility(object sender, EventArgs e)
-        {
-            Console.WriteLine("Doing a thing");
-        }
-
         private Control GenerateEpisodeControl(string file)
         {
+            string[] splitPath = file.Split('\\');
+            string show = splitPath[2];
+            string season = splitPath[3];
+            string episodeName = splitPath[4];
+
+            Color watchedColor = Color.Red;
+            string watchedSymbol = "X";
+
+            query = "SELECT * FROM [dbo].[media] WHERE [Show Name] = @showName AND [Season] = @season AND [File Name] = @fileName";
+            cmd = new SqlCommand(query, conn);
+            cmd.Parameters.Add("@showName", SqlDbType.NVarChar);
+            cmd.Parameters.Add("@season", SqlDbType.NVarChar);
+            cmd.Parameters.Add("@fileName", SqlDbType.NVarChar);
+            cmd.Parameters["@showName"].Value = show;
+            cmd.Parameters["@season"].Value = season;
+            cmd.Parameters["@fileName"].Value = episodeName;
+
+            conn.Open();
+            rdr = cmd.ExecuteReader();
+
+            if (rdr.HasRows)
+            {
+                rdr.Read();
+                bool watched = rdr.GetBoolean(4);
+
+                watchedColor = (watched == true) ? Color.Green : Color.Red;
+                watchedSymbol = (watched == true) ? "O" : "X";
+                conn.Close();
+            }
+            else
+            {
+                conn.Close();
+                query = "INSERT INTO [dbo].[media] ([Show Name], [Season], [File Name], [Watched]) VALUES (@showName, @season, @fileName, @watched)";
+                cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@showName", show);
+                cmd.Parameters.AddWithValue("@season", season);
+                cmd.Parameters.AddWithValue("@fileName", episodeName);
+                cmd.Parameters.AddWithValue("@watched", false);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+
+            
+
             GroupBox g = new GroupBox();
             g.Size = new Size(200, 200);
 
@@ -91,41 +136,72 @@ namespace MediaTracker
             l1.Left = 4;
             l1.Top = 4;
             l1.Size = new Size(20, 20);
-            l1.ForeColor = Color.Red;
+            l1.ForeColor = watchedColor;
             l1.BackColor = Color.Black;
             l1.Font = new Font("Arial", 12);
             l1.TextAlign = ContentAlignment.MiddleCenter;
-            l1.Text = "O";
+            l1.Text = watchedSymbol;
+            l1.Click += (o, i) =>
+            {
+                query = "SELECT [Watched] FROM [dbo].[media] WHERE [Show Name] = @showName AND [Season] = @season AND [File Name] = @fileName";
+                cmd = new SqlCommand(query, conn);
+                cmd.Parameters.Add("@showName", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@season", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@fileName", SqlDbType.NVarChar);
+                cmd.Parameters["@showName"].Value = show;
+                cmd.Parameters["@season"].Value = season;
+                cmd.Parameters["@fileName"].Value = episodeName;
+
+                conn.Open();
+                bool watched = false;
+                rdr = cmd.ExecuteReader();
+                if (rdr.HasRows)
+                {
+                    rdr.Read();
+                    watched = rdr.GetBoolean(0);
+                }
+                conn.Close();
+
+                watched = !watched;
+                watchedColor = (watched == true) ? Color.Green : Color.Red;
+                watchedSymbol = (watched == true) ? "O" : "X";
+                l1.ForeColor = watchedColor;
+                l1.Text = watchedSymbol;
+
+                query = "UPDATE [dbo].[media] SET [Watched] = @watched WHERE [Show Name] = @showName AND [Season] = @season AND [File Name] = @fileName";
+                cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@showName", show);
+                cmd.Parameters.AddWithValue("@season", season);
+                cmd.Parameters.AddWithValue("@fileName", episodeName);
+                cmd.Parameters.AddWithValue("@watched", watched);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                g.Refresh();
+            };
             g.Controls.Add(l1);
-
-            Label l2 = new Label();
-            l2.Left = 27;
-            l2.Top = 4;
-            l2.Size = new Size(26, 20);
-            l2.ForeColor = Color.Red;
-            l2.BackColor = Color.Black;
-            l2.Font = new Font("Arial", 12);
-            l2.TextAlign = ContentAlignment.MiddleCenter;
-            l2.Text = "99";
-            g.Controls.Add(l2);
-
-            Label l3 = new Label();
-            l3.Left = 56;
-            l3.Top = 4;
-            l3.Size = new Size(26, 20);
-            l3.ForeColor = Color.Red;
-            l3.BackColor = Color.Black;
-            l3.Font = new Font("Arial", 12);
-            l3.TextAlign = ContentAlignment.MiddleCenter;
-            l3.Text = "99";
-            g.Controls.Add(l3);
 
             Button play = new Button();
             play.Text = "Play";
             play.FlatStyle = FlatStyle.Popup;
-            play.Left = 85;
+            play.Left = 25;
             play.Top = 2;
-            play.Click += (o, i) => { System.Diagnostics.Process.Start(file); };
+            play.Click += (o, i) =>
+            {
+                query = "UPDATE [dbo].[media] SET [Watched] = 1 WHERE [Show Name] = @showName AND [Season] = @season AND [File Name] = @fileName";
+                cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@showName", show);
+                cmd.Parameters.AddWithValue("@season", season);
+                cmd.Parameters.AddWithValue("@fileName", episodeName);
+                cmd.Parameters.AddWithValue("@watched", false);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                l1.Text = "O";
+                l1.ForeColor = Color.Green;
+                g.Refresh();
+                System.Diagnostics.Process.Start(file);
+            };
             g.Controls.Add(play);
 
             Label l4 = new Label();
