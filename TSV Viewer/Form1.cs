f3f0b204b7569df6cfb6ac101063105dea55f821
thetornadotitan <thetornadotitan@gmail.com>
2018-06-02 02:48:31 -0500

    UI Changes

--- TSV Viewer/Form1.cs
+++ TSV Viewer/Form1.cs
@@ -77,50 +77,73 @@ namespace MediaTracker
             EpisodeFlow.Refresh();
         }
 
+        private void ToggleVisibility(object sender, EventArgs e)
+        {
+            Console.WriteLine("Doing a thing");
+        }
+
         private Control GenerateEpisodeControl(string file)
         {
             GroupBox g = new GroupBox();
             g.Size = new Size(200, 200);
 
             Label l1 = new Label();
-            l1.Location = new Point(2, 2);
+            l1.Left = 4;
+            l1.Top = 4;
             l1.Size = new Size(20, 20);
             l1.ForeColor = Color.Red;
             l1.BackColor = Color.Black;
-            l1.TextAlign = ContentAlignment.TopCenter;
-            l1.Text = "x";
+            l1.Font = new Font("Arial", 12);
+            l1.TextAlign = ContentAlignment.MiddleCenter;
+            l1.Text = "O";
             g.Controls.Add(l1);
 
             Label l2 = new Label();
-            l2.Location = new Point(22, 2);
-            l2.Size = new Size(20, 20);
+            l2.Left = 27;
+            l2.Top = 4;
+            l2.Size = new Size(26, 20);
             l2.ForeColor = Color.Red;
             l2.BackColor = Color.Black;
-            l2.TextAlign = ContentAlignment.TopCenter;
-            l2.Text = "0";
+            l2.Font = new Font("Arial", 12);
+            l2.TextAlign = ContentAlignment.MiddleCenter;
+            l2.Text = "99";
             g.Controls.Add(l2);
 
             Label l3 = new Label();
-            l3.Location = new Point(42, 2);
-            l3.Size = new Size(20, 20);
+            l3.Left = 56;
+            l3.Top = 4;
+            l3.Size = new Size(26, 20);
             l3.ForeColor = Color.Red;
             l3.BackColor = Color.Black;
-            l3.TextAlign = ContentAlignment.TopCenter;
-            l3.Text = "1";
+            l3.Font = new Font("Arial", 12);
+            l3.TextAlign = ContentAlignment.MiddleCenter;
+            l3.Text = "99";
             g.Controls.Add(l3);
 
+            Button play = new Button();
+            play.Text = "Play";
+            play.FlatStyle = FlatStyle.Popup;
+            play.Left = 85;
+            play.Top = 2;
+            play.Click += (o, i) => { System.Diagnostics.Process.Start(file); };
+            g.Controls.Add(play);
+
             Label l4 = new Label();
-            l4.Location = new Point(2, 26);
             l4.ForeColor = Color.White;
             l4.BackColor = Color.FromArgb(200,0,0,0);
+            l4.Size = new Size(200, 200);
+            l4.Font = new Font("Arial", 12);
             string[] s = file.Split('\\');
             l4.Text = s[s.Length-1];
-            l4.Size = new Size(196, 172);
+            l4.TextAlign = ContentAlignment.MiddleCenter;
+            l4.Visible = false;
             g.Controls.Add(l4);
 
             PictureBox p = new PictureBox();
             p.BackColor = Color.DarkGray;
             p.Size = new Size(200, 200);
+            p.MouseEnter += (o, i) => { l4.Visible = true; };
+            p.MouseLeave += (o, i) => { l4.Visible = false; };
             g.Controls.Add(p);
 
             return g;
