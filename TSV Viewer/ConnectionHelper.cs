89603aaafdf9a91d1b7ad40d23593d007ad0a3e4
thetornadotitan <thetornadotitan@gmail.com>
2018-06-03 01:40:05 -0500

    Added Images to UI Added Thumbnails to Episodes

---
+++ TSV Viewer/ConnectionHelper.cs
@@ -0,0 +1,116 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace MediaTracker
+{
+    public static class ConnectionHelper
+    {
+        private static SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\db.mdf;Integrated Security=True");
+        private static string query;
+        private static SqlCommand cmd;
+        private static SqlDataReader rdr;
+
+        private static class Columns
+        {
+            public static int ID = 0;
+            public static int ShowName = 1;
+            public static int Season = 2;
+            public static int FileName = 3;
+            public static int Watched = 4;
+            public static int pic = 5;
+        }
+
+        public static bool ShowExists(string show, string season, string name)
+        {
+            bool result = false;
+
+            query = "SELECT * FROM [dbo].[media] WHERE [Show Name] = @showName AND [Season] = @season AND [File Name] = @fileName";
+            cmd = new SqlCommand(query, conn);
+            cmd.Parameters.Add("@showName", SqlDbType.NVarChar);
+            cmd.Parameters.Add("@season", SqlDbType.NVarChar);
+            cmd.Parameters.Add("@fileName", SqlDbType.NVarChar);
+            cmd.Parameters["@showName"].Value = show;
+            cmd.Parameters["@season"].Value = season;
+            cmd.Parameters["@fileName"].Value = name;
+
+            conn.Open();
+            rdr = cmd.ExecuteReader();
+            result = rdr.HasRows;
+            conn.Close();
+
+            return result;
+        }
+
+        public static bool ShowWatched(string show, string season, string name)
+        {
+            bool result = false;
+
+            query = "SELECT * FROM [dbo].[media] WHERE [Show Name] = @showName AND [Season] = @season AND [File Name] = @fileName";
+            cmd = new SqlCommand(query, conn);
+            cmd.Parameters.Add("@showName", SqlDbType.NVarChar);
+            cmd.Parameters.Add("@season", SqlDbType.NVarChar);
+            cmd.Parameters.Add("@fileName", SqlDbType.NVarChar);
+            cmd.Parameters["@showName"].Value = show;
+            cmd.Parameters["@season"].Value = season;
+            cmd.Parameters["@fileName"].Value = name;
+
+            conn.Open();
+            rdr = cmd.ExecuteReader();
+            rdr.Read();
+            result = rdr.GetBoolean(Columns.Watched);
+            conn.Close();
+
+            return result;
+        }
+
+        public static string GetPic(string show, string season, string name)
+        {
+            string result = "";
+
+            query = "SELECT * FROM [dbo].[media] WHERE [Show Name] = @showName AND [Season] = @season AND [File Name] = @fileName";
+            cmd = new SqlCommand(query, conn);
+            cmd.Parameters.Add("@showName", SqlDbType.NVarChar);
+            cmd.Parameters.Add("@season", SqlDbType.NVarChar);
+            cmd.Parameters.Add("@fileName", SqlDbType.NVarChar);
+            cmd.Parameters["@showName"].Value = show;
+            cmd.Parameters["@season"].Value = season;
+            cmd.Parameters["@fileName"].Value = name;
+
+            conn.Open();
+            rdr = cmd.ExecuteReader();
+            rdr.Read();
+            result = rdr.GetString(Columns.pic);
+            conn.Close();
+
+            return result;
+        }
+
+        public static void AddShow(string show, string season, string name, string pic)
+        {
+            query = "INSERT INTO [dbo].[media] ([Show Name], [Season], [File Name], [Watched], [Pic]) VALUES (@showName, @season, @fileName, @watched, @pic)";
+            cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@showName", show);
+            cmd.Parameters.AddWithValue("@season", season);
+            cmd.Parameters.AddWithValue("@fileName", name);
+            cmd.Parameters.AddWithValue("@watched", false);
+            cmd.Parameters.AddWithValue("@pic", pic);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public static void SetWatched(string show, string season, string name, bool watched)
+        {
+            query = "UPDATE [dbo].[media] SET [Watched] = @watched WHERE [Show Name] = @showName AND [Season] = @season AND [File Name] = @fileName";
+            cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@showName", show);
+            cmd.Parameters.AddWithValue("@season", season);
+            cmd.Parameters.AddWithValue("@fileName", name);
+            cmd.Parameters.AddWithValue("@watched", watched);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+    }
+}
---
+++ TSV Viewer/EpisodeControl.cs
@@ -0,0 +1,103 @@
+﻿using NReco.VideoConverter;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MediaTracker
+{
+    class EpisodeControl : Panel
+    {
+        public EpisodeControl(string file)
+        {
+            string[] splitPath = file.Split('\\');
+            string show = splitPath[2];
+            string season = splitPath[3];
+            string episodeName = splitPath[4];
+            bool watched = false;
+
+            BackColor = Color.DarkGray;
+            BackgroundImageLayout = ImageLayout.Stretch;
+
+            if (ConnectionHelper.ShowExists(show, season, episodeName))
+            {
+                watched = ConnectionHelper.ShowWatched(show, season, episodeName);
+                try
+                {
+                    BackgroundImage = Image.FromFile(ConnectionHelper.GetPic(show, season, episodeName));
+                }
+                catch { }
+            }
+            else
+            {
+                if (!Directory.Exists(Path.Combine(Directory.GetCurrentDirectory(), "resources", "Thumbnails"))){
+                    Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "resources", "Thumbnails"));
+                }
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "resources", "Thumbnails", episodeName + ".jpg");
+                FFMpegConverter ffMpeg = new FFMpegConverter();
+                Stream f = new MemoryStream();
+
+                try
+                {
+                    ffMpeg.GetVideoThumbnail(file, f, 60);
+                    Image i = Image.FromStream(f);
+                    BackgroundImage = i;
+                    i.Save(path, ImageFormat.Jpeg);
+                }
+                catch { }
+
+                ConnectionHelper.AddShow(show, season, episodeName, path);
+            }
+
+            Size = new Size(200, 200);
+
+            PictureBox l1 = new PictureBox();
+            l1.Left = 4;
+            l1.Top = 4;
+            l1.Size = new Size(25, 25);
+            l1.BackgroundImage = Image.FromFile(Path.Combine(Directory.GetCurrentDirectory(), "resources", (watched) ? "Watched.png" : "Not Watched.png"));
+            l1.BackgroundImageLayout = ImageLayout.Stretch;
+            l1.BackColor = Color.Transparent;
+            l1.Click += (o, i) =>
+            {
+                watched = !watched;
+                ConnectionHelper.SetWatched(show, season, episodeName, watched);
+                l1.BackgroundImage = Image.FromFile(Path.Combine(Directory.GetCurrentDirectory(), "resources", (watched) ? "Watched.png" : "Not Watched.png"));
+                Refresh();
+            };
+            Controls.Add(l1);
+
+            PictureBox play = new PictureBox();
+            play.Text = "Play";
+            play.Size = new Size(35, 25);
+            play.Left = 35;
+            play.Top = 4;
+            play.BackgroundImage = Image.FromFile(Path.Combine(Directory.GetCurrentDirectory(), "resources", "Play.png"));
+            play.BackgroundImageLayout = ImageLayout.Stretch;
+            play.BackColor = Color.Transparent;
+            play.Click += (o, i) =>
+            {
+                ConnectionHelper.SetWatched(show, season, episodeName, true);
+                l1.Text = "O";
+                l1.ForeColor = Color.Green;
+                Refresh();
+                System.Diagnostics.Process.Start(file);
+            };
+            Controls.Add(play);
+
+            Label l4 = new Label();
+            l4.ForeColor = Color.White;
+            l4.BackColor = Color.FromArgb(200, 0, 0, 0);
+            l4.Size = new Size(200, 200);
+            l4.Font = new Font("Arial", 12);
+            string[] s = file.Split('\\');
+            l4.Text = s[s.Length - 1];
+            l4.TextAlign = ContentAlignment.MiddleCenter;
+            l4.Visible = false;
+            l4.MouseLeave += (o, i) => { l4.Visible = false; };
+            Controls.Add(l4);
+            
+            MouseEnter += (o, i) => { l4.Visible = true; };
+        }
+    }
+}
