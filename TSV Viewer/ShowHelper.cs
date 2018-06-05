a0de330c7ed2e5e2695fcf7e4b44eb6e31419117
thetornadotitan <thetornadotitan@gmail.com>
2018-06-05 02:52:57 -0500

    Show Only Video Files (Checking Extension)

--- TSV Viewer/ShowHelper.cs
+++ TSV Viewer/ShowHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,31 @@ namespace MediaTracker
 {
     public static class ShowHelper
     {
+        private static List<string> videoFiles = new List<string>()
+        {
+            ".webm", ".mkv", ".flv", ".vob", ".ogv", ".ogg", ".drc",
+            ".gif", ".avi", ".mov", ".qt", ".wmv", ".mng", ".yuv",
+            ".rm", ".rmvb", ".asf", ".mp4", ".m4p", ".m4v", ".mpg",
+            ".mp2", ".mpeg", ".mpe", ".mpv", ".m2v", ".m4v", ".svi",
+            ".3gp", ".3g2", ".mxf", ".roq", ".nsv", ".f4v", ".f4p",
+            ".f4a", ".f4b"
+        };
+
         public static string[] GetShowInfo(string file)
         {
             string[] splitPath = file.Split('\\');
             string[] result = { splitPath[2], splitPath[3], splitPath[4] };
             return result;
+        }
+
+        public static bool IsShow(string file)
+        {
+            if (File.Exists(file))
+            {
+                return videoFiles.Contains(Path.GetExtension(file));
+            }
+
+            return false;
         }
     }
 }
